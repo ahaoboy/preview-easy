b38f1deed29030423c2f3f43e63b896afa9ace42
ahaoboy <504595380@qq.com>
2024-10-23 16:06:06 +0800

    support rar

--- src/rules/zip.ts
+++ src/rules/zip.ts
@@ -1,8 +1,11 @@
 import type { Rule } from "../type"
-import fs from "fs"
 import path from "path"
 import AdmZip from "adm-zip"
-import unrar from 'node-unrar-js'
+import { createExtractorFromFile } from "node-unrar-js/esm"
+import rarWasmBase64 from "node-unrar-js/esm/js/unrar.wasm"
+import * as tar from "tar"
+import fs from "fs"
+import _7z from "7zip-min"
 
 type Info = {
   name: string
@@ -60,11 +63,13 @@ async function getInfo(filePath: string): Promise<Info[]> {
       })
     }
   } else if (filePath.endsWith(".rar")) {
-    const buf = Uint8Array.from(fs.readFileSync(filePath)).buffer;
-    const extractor = await unrar.createExtractorFromData({ data: buf });
-    const fileList = extractor.getFileList();
-
-    for(const entry of fileList.fileHeaders){
+    const wasmBinary = Buffer.from(rarWasmBase64, "base64")
+    const extractor = await createExtractorFromFile({
+      wasmBinary,
+      filepath: filePath,
+    })
+    const fileList = extractor.getFileList()
+    for (const entry of [...fileList.fileHeaders]) {
       list.push({
         name: entry.name,
         path: entry.name,
@@ -72,12 +77,48 @@ async function getInfo(filePath: string): Promise<Info[]> {
         time: new Date(entry.time),
         size: entry.unpSize,
         compressedSize: entry.packSize,
-        isDirectory: entry.flags.directory
+        isDirectory: entry.flags.directory,
       })
     }
   } else if (filePath.endsWith(".7z")) {
-  } else if (filePath.endsWith(".tar")) {
-  } else if (filePath.endsWith(".gz")) {
+    _7z.list("./test.7z", (err, result) => {
+      if (err) {
+        return []
+      }
+      for (const entry of result) {
+        list.push({
+          name: entry.name,
+          path: entry.name,
+          mode: 0,
+          time: new Date(entry.time),
+          size: +entry.size,
+          compressedSize: +entry.compressed,
+          isDirectory: false,
+        })
+      }
+    })
+  } else if (filePath.endsWith(".tar") || filePath.endsWith(".gz")) {
+    return await new Promise((resolve, reject) => {
+      fs.createReadStream(filePath)
+        .pipe(tar.t())
+        .on("entry", (entry) => {
+          list.push({
+            name: entry.path,
+            size: entry.size,
+            path: entry.path,
+            mode: 0,
+            time: new Date(0),
+            compressedSize: 0,
+            isDirectory: entry.type === "Directory",
+          })
+        })
+        .on("finish", () => {
+          resolve(list)
+        })
+        .on("error", (error) => {
+          reject(`Error: ${error.message}`)
+        })
+    })
   }
   return list
 }
@@ -89,7 +130,7 @@ export const Zip: Rule = {
     return [".zip", ".rar", ".7z", ".tar", ".gz"].includes(fileExtension)
   },
   async render(filePath: string) {
-    const list =await getInfo(filePath)
+    const list = await getInfo(filePath)
     const html = renderTree(list)
     return html
   },
